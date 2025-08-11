ae755e821c7966fda9fbc11e56da1e1010bea2e5
ediskrad007 <lysophearak9999gmail.com>
2025-08-11 11:20:26 +0700

    second

---
+++ week2/lomat2.js
@@ -0,0 +1,7 @@
+function rant(message) {
+    const upperMessage = message.toUpperCase();
+    console.log(upperMessage);
+    console.log(upperMessage);
+    console.log(upperMessage);
+}
+rant("I hate beets");
